7a30a0e30ab3631f3d167b74a3e5b46513ac4751
Caroline Harrison <ishimaru.chiaki@gmail.com>
2011-12-09 03:31:19 -0500

    Annulation de l’envoi du dossier bak envoyé par erreur

--- bak/admin/templates/admin_overview.php
+++
@@ -1,6 +0,0 @@
-		<div class="centerformtext">
-			<?php echo lang('pa_o_welcome'); ?>
-		</div><br />
-		<div class="centerformtext">
-			<?php echo lang(array('item' => $o_newversion)); ?>
-		</div>
